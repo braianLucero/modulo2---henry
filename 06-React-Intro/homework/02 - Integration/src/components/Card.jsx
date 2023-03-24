dfd81c6afc42887e94393dca0b157b9f5b7fc6c9
brian Lucero <brasanluc123@gmail.com>
2023-03-24 16:02:06 -0300

    app-Ricky_Morthy

--- 06-React-Intro/homework/02 - Integration/src/components/Card.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Card.jsx
@@ -1,13 +1,16 @@
-export default function Card(props) {
+import './Card.css'
+export default function Card({id,name,status,species,gender,origin,image,onClose}) {
    return (
-      <div>
-         {/* <button onClick={}>X</button>
-         <h2></h2>
-         <h2></h2>
-         <h2></h2>
-         <h2></h2>
-         <h2></h2>
-         <img src={} alt='' /> */}
+      <div className="card">
+               <button onClick={onClose}>X</button>
+               <h2>{name}</h2>
+               <h2>{status}</h2>
+               <h2>{species}</h2>
+               <h2>{gender}</h2>
+               <h2>{origin}</h2> 
+               <img src={image} alt= {name} />
       </div>
    );
 }
+
+
--- 06-React-Intro/homework/02 - Integration/src/components/SearchBar.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/SearchBar.jsx
@@ -1,8 +1,9 @@
-export default function SearchBar(props) {
+
+export default function SearchBar({onSearch}) {
    return (
       <div>
-         {/* <input type='search' />
-         <button onClick={}>Agregar</button> */}
+            <input type='search' />
+            <button onClick={() => onSearch('no existe el ID   ')}>Agregar</button>
       </div>
    );
 }
