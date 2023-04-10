b6941026e2256337141fae70e7c96912438dde98
brian Lucero <brasanluc123@gmail.com>
2023-04-09 22:19:21 -0300

    homework-hooks

--- 13-React-Hooks/homework/01 - Exercises/src/components/ContactUs/ContactUs.jsx
+++ 13-React-Hooks/homework/01 - Exercises/src/components/ContactUs/ContactUs.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 
 const ContactUs = () => {
+  const [form, setForm] = React.useState({
+    nombre: "",
+    email: "",
+    asunto: "",
+    mensaje: "",
+  });
+
+  function handleInput(evento) {
+    setForm({ ...form, [evento.target.name]: evento.target.value });
+  }
   return (
     <div>
       <form className="contactBg">
         <label htmlFor="nombre">Nombre: </label>
-        <input name="nombre" />
+        <input name="nombre" value={form.nombre} onChange={handleInput} />
         <label htmlFor="email">Email: </label>
-        <input name="email" />
+        <input name="email" value={form.email} onChange={handleInput} />
         <label htmlFor="asunto">Asunto: </label>
-        <input name="asunto" />
+        <input name="asunto" value={form.asunto} onChange={handleInput} />
         <label htmlFor="mensaje">Mensaje: </label>
-        <input name="mensaje" />
+        <input name="mensaje" value={form.mensaje} onChange={handleInput} />
         <button>Enviar</button>
       </form>
     </div>
